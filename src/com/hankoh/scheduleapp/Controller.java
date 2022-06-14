6a2ee3c726d3c2d10729ba5616050982b69704e3
hkoh2 <hkoh2@wgu.edu>
2022-06-13 19:54:57 -0500

    JDBC connection

---
+++ src/com/hankoh/scheduleapp/Controller.java
@@ -0,0 +1,4 @@
+package com.hankoh.scheduleapp;
+
+public class Controller {
+}
---
+++ src/com/hankoh/scheduleapp/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.hankoh.scheduleapp.controller;
+
+public class MainController {
+    public void initialize() {
+
+    }
+}
