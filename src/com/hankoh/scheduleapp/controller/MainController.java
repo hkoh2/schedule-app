30e75f43da6cfa6bc0cd2b8da190c1e1de0fd5f6
hkoh2 <hkoh2@wgu.edu>
2022-06-17 18:42:11 -0500

    Log out button implemented.

--- src/com/hankoh/scheduleapp/controller/MainController.java
+++ src/com/hankoh/scheduleapp/controller/MainController.java
@@ -2,11 +2,17 @@ package com.hankoh.scheduleapp.controller;
 
 import com.hankoh.scheduleapp.model.DataStorage;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -70,7 +76,20 @@ public class MainController {
     public void onDeleteCustomerButtonClick(ActionEvent actionEvent) {
     }
 
-    public void onLogoutButtonClick(ActionEvent actionEvent) {
+    public void onLogoutButtonClick(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(msg.getString("logout"));
+        alert.setHeaderText(msg.getString("logout"));
+        alert.setContentText(msg.getString("logout_msg"));
+        Optional<ButtonType> choice = alert.showAndWait();
+        if (choice.isPresent() && choice.get() == ButtonType.OK) {
+            DataStorage.getInstance().clearAll();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/hankoh/scheduleapp/view/login.fxml")));
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle(msg.getString("login.title"));
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
     }
 
     public void onExitButtonClick(ActionEvent actionEvent) {
